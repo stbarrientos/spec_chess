cc3b7686265ee705a7e3a367e4755fe1b44deec9
stbarrientos <stbarrientos@gmail.com>
2014-09-10 18:04:44 -0400

    javascript fix for mirror.js

--- app/assets/javascripts/customize.js
+++ app/assets/javascripts/customize.js
@@ -1,11 +1,33 @@
-var editor = CodeMirror.fromTextArea(document.getElementById("test_code"), {
+if(document.getElementById("test_code")){
+  var editor = CodeMirror.fromTextArea(document.getElementById("test_code"), {
   mode: "text/x-ruby",
   matchBrackets: true,
   indentUnit: 4
 });
+}
 
-var editor = CodeMirror.fromTextArea(document.getElementById("solution_code"), {
+if(document.getElementById("solution_code")){
+  var editor = CodeMirror.fromTextArea(document.getElementById("solution_code"), {
   mode: "text/x-ruby",
   matchBrackets: true,
   indentUnit: 4
 });
+}
+
+if(document.getElementById("display_solution_code")){
+  var editor = CodeMirror.fromTextArea(document.getElementById("display_solution_code"), {
+  mode: "text/x-ruby",
+  matchBrackets: true,
+  indentUnit: 4,
+  readOnly: true
+});
+}
+
+if(document.getElementById("display_test_code")){
+  var editor = CodeMirror.fromTextArea(document.getElementById("display_test_code"), {
+  mode: "text/x-ruby",
+  matchBrackets: true,
+  indentUnit: 4,
+  readOnly: true
+});
+}
